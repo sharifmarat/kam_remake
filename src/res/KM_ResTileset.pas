f006c4f2c055693dbd11b059ae10154c44d2db06
Rey <levchenko.and@gmail.com>
2017-10-05 20:33:18 +0300

    Fix ice mask animation

--- src/res/KM_ResTileset.pas
+++ src/res/KM_ResTileset.pas
@@ -65,7 +65,7 @@ type
     tkCobbleStone,
     tkGrassyWater,//12
     tkSwamp,      //13
-    tkIce,
+    tkIce,        //14
     tkShallowSnow,
     tkSnow,
     tkDeepSnow,
@@ -85,8 +85,10 @@ const
   TER_KIND_ORDER: array[tkCustom..tkLava] of Integer =
     (0,1,2,3,4,5,6,7,8,9,10,11,
       -1,    // To make Water/FastWater-GrassyWater transition possible with layers we need GrassyWater to be above Water because of animation (water above grassy anim looks ugly)
-      13,14,15,16,17,18,19,20,21,22,23,24,25,
-      -3,-2, // Put GrassyWater/Water/FastWater always to the base layer, because of animation
+      13,
+      -2,
+      15,16,17,18,19,20,21,22,23,24,25,
+      -4,-3, // Put GrassyWater/Water/FastWater always to the base layer, because of animation
       28);
 
   BASE_TERRAIN: array[TKMTerrainKind] of Word = //tkCustom..tkLava] of Word =
