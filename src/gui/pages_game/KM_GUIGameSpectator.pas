13e0324d7e975725245c95549b198bd0fd361dc3
ZblCoder <zblcoder@gmail.com>
2017-12-12 03:39:00 +0300

    Added Buildings panel

--- src/gui/pages_game/KM_GUIGameSpectator.pas
+++ src/gui/pages_game/KM_GUIGameSpectator.pas
@@ -19,8 +19,13 @@ uses
   {$IFDEF Unix} LCLIntf, LCLType, {$ENDIF}
   Classes, Math, StrUtils, SysUtils,
   KM_Controls, KM_HandsCollection, KM_Defaults, KromOGLUtils, KM_Hand, KM_Units,
-  KM_ResWares;
+  KM_ResWares, KM_ResHouses, KM_Pics;
 
+const
+  GUI_SPECTATOR_ITEM_WIDTH = 28;
+  GUI_SPECTATOR_ITEM_HEIGHT = 34;
+  GUI_SPECTATOR_ITEM_SPLITE_H = 4;
+  GUI_SPECTATOR_ITEM_SPLITE_V = 4;
 
 type
   TKMGUIGameSpectatorPageTypes = (
@@ -33,65 +38,77 @@ type
     gsptLosses = 6
   );
 
-  TKMGUIGameSpectatorItem = class
+  TKMGUIGameSpectatorItem = class(TKMControl)
   private
-    FImage: TKMImage;
-    FLabel: TKMLabel;
+    FHandIndex: Integer;
+    FImageID: Word;
+    FValue: String;
     FTag: Integer;
-
-    function GetValue: String;
-    procedure SetValue(AValue: String);
-    function GetVisible: Boolean;
-    procedure SetVisible(AValue: Boolean);
   public
-    constructor Create(aParent: TKMPanel; ATag: Integer; AImage: Word; AHint: String; AColor: TColor4);
-    procedure SetPosition(ALeft, ATop: Integer);
+    constructor Create(aParent: TKMPanel; ATag: Integer; AImageID: Word; AHint: String; AHandIndex: Integer);
     property Tag: Integer read FTag;
-    property Value: String read GetValue write SetValue;
-    property Visible: Boolean read GetVisible write SetVisible;
+    property Value: String read FValue write FValue;
+    procedure Paint; override;
   end;
 
-  TKMGUIGameSpectatorPanel = class(TKMPanel)
+  TKMGUIGameSpectatorItemLineClass = class of TKMGUIGameSpectatorItemLine;
+
+  TKMGUIGameSpectatorItemLine = class(TKMPanel)
+  protected
+    FHandIndex: Integer;
+    FItems: array of TKMGUIGameSpectatorItem;
+  protected
+    function CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem; virtual; abstract;
+    function GetTagCount: Integer; virtual; abstract;
+    function GetTag(AIndex: Integer): Integer; virtual; abstract;
+    function GetValue(AHandIndex: Integer; ATag: Integer): String; virtual; abstract;
   public
-    constructor Create(aParent: TKMPanel); virtual;
-    procedure Activation; virtual;
-
-  end;
-
-  TKMGUIGameSpectatorPanelArmy = class(TKMGUIGameSpectatorPanel)
-  private
-    fItems: array of array[WARRIOR_MIN..WARRIOR_MAX] of TKMGUIGameSpectatorItem;
-  public
-    constructor Create(aParent: TKMPanel); override;
-    procedure Activation; override;
-
-  end;
-
-  TKMGUIGameSpectatorPanelResources = class(TKMGUIGameSpectatorPanel)
-  private
-    fItems: array of array[WARE_MIN..WARE_MAX] of TKMGUIGameSpectatorItem;
-  public
-    constructor Create(aParent: TKMPanel); override;
-    procedure Activation; override;
+    constructor Create(aParent: TKMPanel; AHandIndex: Integer);
     procedure UpdateState(aTickCount: Cardinal); override;
   end;
 
+  ///
+
+  TKMGUIGameSpectatorItemLineResources = class(TKMGUIGameSpectatorItemLine)
+  protected
+    function CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem; override;
+    function GetTagCount: Integer; override;
+    function GetTag(AIndex: Integer): Integer; override;
+    function GetValue(AHandIndex: Integer; ATag: Integer): String; override;
+  end;
+
+  TKMGUIGameSpectatorItemLineBuildings = class(TKMGUIGameSpectatorItemLine)
+  protected
+    function CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem; override;
+    function GetTagCount: Integer; override;
+    function GetTag(AIndex: Integer): Integer; override;
+    function GetValue(AHandIndex: Integer; ATag: Integer): String; override;
+  end;
+
+  TKMGUIGameSpectatorItemLineArmy = class(TKMGUIGameSpectatorItemLine)
+  protected
+    function CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem; override;
+    function GetTagCount: Integer; override;
+    function GetTag(AIndex: Integer): Integer; override;
+    function GetValue(AHandIndex: Integer; ATag: Integer): String; override;
+  end;
+
+  ///
+
   TKMGUIGameSpectator = class
   private
-    FPanel: TKMPanel;
+    FDropBoxPanel: TKMPanel;
     FDropBox: TKMDropList;
-    FSelectedPanel: TKMGUIGameSpectatorPanel;
-    FPanelList: TList;
+    FLastIndex: Integer;
 
-    //TKMGUIGameSpectatorArmy;
+    FLines: array of array[0..MAX_LOBBY_PLAYERS] of TKMGUIGameSpectatorItemLine;
 
+    procedure AddLineType(AIndex: Integer; ALineClass: TKMGUIGameSpectatorItemLineClass);
     procedure ChangePage(Sender: TObject);
-    procedure AddPage(AName: String; APanel: TKMGUIGameSpectatorPanel);
   public
     constructor Create(aParent: TKMPanel);
-    destructor Destroy; override;
-
   end;
+
 implementation
 
 uses
@@ -99,181 +116,129 @@ uses
 
 { TKMGUIGameSpectatorItem }
 
-constructor TKMGUIGameSpectatorItem.Create(aParent: TKMPanel; ATag: Integer; AImage: Word; AHint: String; AColor: TColor4);
+constructor TKMGUIGameSpectatorItem.Create(aParent: TKMPanel; ATag: Integer; AImageID: Word; AHint: String; AHandIndex: Integer);
 begin
+  inherited Create(aParent, 0, 0, GUI_SPECTATOR_ITEM_WIDTH, GUI_SPECTATOR_ITEM_HEIGHT);
   FTag := ATag;
-
-  FImage := TKMImage.Create(aParent, 0, 0, 30, 30, AImage);
-  FImage.Hint := AHint;
-  FImage.FlagColor := AColor;
-  FImage.ImageCenter;
-
-  FLabel := TKMLabel.Create(aParent, 0, 0, '0', fnt_Grey, taRight);
-  FLabel.Hitable := False;
+  Hint := AHint;
+  FHandIndex := AHandIndex;
+  FImageID := AImageID;
 end;
 
-procedure TKMGUIGameSpectatorItem.SetPosition(ALeft, ATop: Integer);
+procedure TKMGUIGameSpectatorItem.Paint;
 begin
-  FImage.Left := ALeft;
-  FImage.Top := ATop;
-  FLabel.Left := ALeft + 30;
-  FLabel.Top := ATop + 16;
+  inherited;
+
+  TKMRenderUI.WriteBevel(AbsLeft, AbsTop, Width, Height);
+  TKMRenderUI.WritePicture(AbsLeft + 2, AbsTop, Width - 4, Height - 4, [], rxGui, FImageID, True, gHands[FHandIndex].FlagColor);
+  TKMRenderUI.WriteText(AbsLeft, AbsTop + Height - 14, Width, FValue, fnt_Grey, taCenter, $FFFFFFFF);
 end;
 
-function TKMGUIGameSpectatorItem.GetValue: String;
-begin
-  Result := FLabel.Caption;
-end;
+{ TKMGUIGameSpectatorItemLine }
 
-procedure TKMGUIGameSpectatorItem.SetValue(AValue: String);
+constructor TKMGUIGameSpectatorItemLine.Create(aParent: TKMPanel; AHandIndex: Integer);
+var
+  i: Integer;
 begin
-  FLabel.Caption := AValue;
-end;
-
-function TKMGUIGameSpectatorItem.GetVisible: Boolean;
-begin
-  Result := FImage.Visible;
-end;
-
-procedure TKMGUIGameSpectatorItem.SetVisible(AValue: Boolean);
-begin
-  FImage.Visible := AValue;
-  FLabel.Visible := AValue;
-end;
-
-{ TKMGUIGameSpectatorPanel }
-
-constructor TKMGUIGameSpectatorPanel.Create(aParent: TKMPanel);
-begin
-  inherited Create(aParent, aParent.Width - 160, 32, 160, 30);
+  inherited Create(aParent, aParent.Width, 32 + AHandIndex * (GUI_SPECTATOR_ITEM_HEIGHT + GUI_SPECTATOR_ITEM_SPLITE_V), 0, GUI_SPECTATOR_ITEM_HEIGHT);
   Anchors := [anTop, anRight];
-  Focusable := false;
-  Hide;
-
-
+  Focusable := false;  
+  FHandIndex := AHandIndex;
+  SetLength(fItems, GetTagCount);
+  for i := 0 to GetTagCount - 1 do
+    fItems[i] := CreateItem(AHandIndex, GetTag(i));
 end;
 
-procedure TKMGUIGameSpectatorPanel.Activation;
-begin
-
-end;
-
-{ TKMGUIGameSpectatorPanelResources }
-
-constructor TKMGUIGameSpectatorPanelResources.Create(aParent: TKMPanel);
-begin
-  inherited;
-
-end;
-
-procedure TKMGUIGameSpectatorPanelResources.Activation;
+procedure TKMGUIGameSpectatorItemLine.UpdateState(aTickCount: Cardinal);
 var
-  i, p, HandCount, WareCount: Integer;
-  WT: TWareType;
+  i, Position: Integer;
+  Value: String;
 begin
   inherited;
-  HandCount := Min(MAX_LOBBY_PLAYERS, gHands.Count);
-  if HandCount = Length(fItems) then
+  if not Visible then
     Exit;
 
-  SetLength(fItems, HandCount);
-  Height := 32 * HandCount;
-  Width := 448;
-  Left := Parent.Width - 448;
-  for i := 0 to HandCount - 1 do
+  Position := 0;
+  for i := 0 to GetTagCount - 1 do
   begin
-    p := 0;
-    for WT := WARE_MIN to WARE_MAX do
-    begin
-      WareCount := gHands[i].Stats.GetWareBalance(WT);
-      if WareCount > 0 then
-        p := p + 32;
-      fItems[i, WT] := TKMGUIGameSpectatorItem.Create(Self, Integer(WT), gRes.Wares[WT].GUIIcon, gRes.Wares[WT].Title, gHands[i].FlagColor);
-      fItems[i, WT].SetPosition(Width - p, i * 32);
-      fItems[i, WT].Value := IntToStr(WareCount);
-      fItems[i, WT].Visible := WareCount > 0;
-    end;
+    Value := GetValue(FHandIndex, GetTag(i));
+    fItems[i].Visible := Value <> '0';
+    fItems[i].Top := 0;
+    fItems[i].Left := Position;
+    fItems[i].Value := Value;
+    if fItems[i].Visible then
+      Inc(Position, GUI_SPECTATOR_ITEM_WIDTH + GUI_SPECTATOR_ITEM_SPLITE_H);
   end;
+  Left := Parent.Width - Position;
 end;
 
-procedure TKMGUIGameSpectatorPanelResources.UpdateState(aTickCount: Cardinal);
-var
-  i, p, HandCount, WareCount: Integer;
-  WT: TWareType;
+{ TKMGUIGameSpectatorItemLineResources }
+
+function TKMGUIGameSpectatorItemLineResources.CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem;
 begin
-  inherited;
-
-  if Length(fItems) = 0 then
-    Exit;
-
-  for i := 0 to HandCount - 1 do
-  begin
-    p := 0;
-    for WT := WARE_MIN to WARE_MAX do
-    begin
-      WareCount := gHands[i].Stats.GetWareBalance(WT);
-
-      if WareCount > 0 then
-        p := p + 32;
-
-      fItems[i, WT].SetPosition(Width - p, i * 32);
-      fItems[i, WT].Value := IntToStr(WareCount);
-      fItems[i, WT].Visible := WareCount > 0;
-    end;
-  end;
+  Result := TKMGUIGameSpectatorItem.Create(Self, ATag, gRes.Wares[TWareType(ATag)].GUIIcon, gRes.Wares[TWareType(ATag)].Title, FHandIndex);
+  Result.Visible := False;
 end;
 
-{ TKMGUIGameSpectatorPanelArmy }
-
-constructor TKMGUIGameSpectatorPanelArmy.Create(aParent: TKMPanel);
+function TKMGUIGameSpectatorItemLineResources.GetTagCount: Integer;
 begin
-  inherited;
-
+  Result := Integer(WARE_MAX) - Integer(WARE_MIN);
 end;
 
-procedure TKMGUIGameSpectatorPanelArmy.Activation;
-
-  function GetUnitCount(AHand: TKMHand; AUnitType: TUnitType): Integer;
-  var
-    i: Integer;
-  begin
-    Result := 0;
-    for i := 0 to AHand.Units.Count - 1 do
-      if AHand.Units[i].UnitType = AUnitType then
-        Inc(Result);
-  end;
-var
-  i, p, HandCount: Integer;
-  WT: TUnitType;
-  UnitCount: Integer;
+function TKMGUIGameSpectatorItemLineResources.GetTag(AIndex: Integer): Integer;
 begin
-  inherited;
+  Result := Integer(WARE_MIN) + AIndex;
+end;
 
-  HandCount := Min(MAX_LOBBY_PLAYERS, gHands.Count);
+function TKMGUIGameSpectatorItemLineResources.GetValue(AHandIndex: Integer; ATag: Integer): String;
+begin
+  Result := IntToStr(gHands[AHandIndex].Stats.GetWareBalance(TWareType(ATag)));
+end;
 
-  if HandCount = Length(fItems) then
-    Exit;
+{ TKMGUIGameSpectatorItemLineBuildings }
 
-  SetLength(fItems, HandCount);
-  Height := 32 * HandCount;
-  Width := 448;
-  Left := Parent.Width - 448;
-  for i := 0 to HandCount - 1 do
-  begin
-    p := 0;
-    for WT := WARRIOR_MIN to WARRIOR_MAX do
-    begin
-      UnitCount := GetUnitCount(gHands[i], WT);
-      if UnitCount > 0 then
-      begin
-        p := p + 32;
-        fItems[i, WT] := TKMGUIGameSpectatorItem.Create(Self, Integer(WT), gRes.Units[WT].GUIIcon, gRes.Units[WT].GUIName, gHands[i].FlagColor);
-        fItems[i, WT].SetPosition(Width - p, i * 32);
-        fItems[i, WT].Value := IntToStr(UnitCount);
-      end;
-    end;
-  end;
-  //gHands
+function TKMGUIGameSpectatorItemLineBuildings.CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem;
+begin
+  Result := TKMGUIGameSpectatorItem.Create(Self, ATag, gRes.Houses[THouseType(ATag)].GUIIcon, gRes.Houses[THouseType(ATag)].HouseName, FHandIndex);
+  Result.Visible := False;
+end;
+
+function TKMGUIGameSpectatorItemLineBuildings.GetTagCount: Integer;
+begin
+  Result := Integer(HOUSE_MAX) - Integer(HOUSE_MIN);
+end;
+
+function TKMGUIGameSpectatorItemLineBuildings.GetTag(AIndex: Integer): Integer;
+begin
+  Result := Integer(HOUSE_MIN) + AIndex;
+end;
+
+function TKMGUIGameSpectatorItemLineBuildings.GetValue(AHandIndex: Integer; ATag: Integer): String;
+begin
+  Result := IntToStr(gHands[AHandIndex].Stats.GetHouseQty(THouseType(ATag)));
+end;
+
+{ TKMGUIGameSpectatorItemLineArmy }
+
+function TKMGUIGameSpectatorItemLineArmy.CreateItem(AHandIndex: Integer; ATag: Integer): TKMGUIGameSpectatorItem;
+begin
+  Result := TKMGUIGameSpectatorItem.Create(Self, ATag, gRes.Units[TUnitType(ATag)].GUIIcon, gRes.Units[TUnitType(ATag)].GUIName, FHandIndex);
+  Result.Visible := False;
+end;
+
+function TKMGUIGameSpectatorItemLineArmy.GetTagCount: Integer;
+begin
+  Result := Integer(WARRIOR_MAX) - Integer(WARRIOR_MIN);
+end;
+
+function TKMGUIGameSpectatorItemLineArmy.GetTag(AIndex: Integer): Integer;
+begin
+  Result := Integer(WARRIOR_MIN) + AIndex;
+end;
+
+function TKMGUIGameSpectatorItemLineArmy.GetValue(AHandIndex: Integer; ATag: Integer): String;
+begin
+  Result := IntToStr(gHands[AHandIndex].Stats.GetUnitQty(TUnitType(ATag)));
 end;
 
 { TKMGUIGameSpectator }
@@ -282,50 +247,64 @@ constructor TKMGUIGameSpectator.Create(aParent: TKMPanel);
 begin
   inherited Create;
 
-  FPanel := TKMPanel.Create(aParent, aParent.Width - 160, 0, 160, 30);
-  FPanel.Anchors := [anTop, anRight];
-  FPanel.Focusable := false;
-  FPanel.Show;
-  FPanelList := TList.Create;
-  FSelectedPanel := nil;
+  FDropBoxPanel := TKMPanel.Create(aParent, aParent.Width - 210, 0, 210, 30);
+  FDropBoxPanel.Anchors := [anTop, anRight];
+  //FDropBoxPanel.Focusable := false;
+  FDropBoxPanel.Show;
 
-  FDropBox := TKMDropList.Create(FPanel, 5, 5, 150, 20, fnt_Metal, '', bsMenu);
+
+  FLastIndex := 0; 
+    
+  SetLength(FLines, 7);       
+     
+  AddLineType(0, nil);
+  AddLineType(1, TKMGUIGameSpectatorItemLineResources);
+  AddLineType(2, TKMGUIGameSpectatorItemLineBuildings);
+  AddLineType(3, nil);
+  AddLineType(4, TKMGUIGameSpectatorItemLineArmy);
+  AddLineType(5, nil);
+  AddLineType(6, nil);
+                 
+  FDropBox := TKMDropList.Create(FDropBoxPanel, 5, 5, 200, 20, fnt_Metal, '', bsGame);
   FDropBox.OnChange := ChangePage;
-
-  AddPage('Ничего', nil);
-  AddPage('Ресурсы', TKMGUIGameSpectatorPanelResources.Create(FPanel));
-  AddPage('Строения', nil);
-  AddPage('Производство', nil);
-  AddPage('Армия', TKMGUIGameSpectatorPanelArmy.Create(FPanel));
-  AddPage('Население', nil);
-  AddPage('Потери', nil);
+  
+  FDropBox.Add('Ничего');  
+  FDropBox.Add('Ресурсы');
+  FDropBox.Add('Строения');
+  FDropBox.Add('Производство');
+  FDropBox.Add('Армия');
+  FDropBox.Add('Население');  
+  FDropBox.Add('Потери');
+  
+  FDropBox.ItemIndex := 0;
 end;
 
-destructor TKMGUIGameSpectator.Destroy;
+procedure TKMGUIGameSpectator.AddLineType(AIndex: Integer; ALineClass: TKMGUIGameSpectatorItemLineClass);
+var
+  i: Integer;
 begin
-  FPanelList.Free;
-
-  inherited;
-end;
-
-procedure TKMGUIGameSpectator.AddPage(AName: String; APanel: TKMGUIGameSpectatorPanel);
-begin
-  FDropBox.Add(AName);
-  FPanelList.Add(APanel);
+  if ALineClass <> nil then  
+    for i := 0 to MAX_LOBBY_PLAYERS - 1 do
+    begin     
+      FLines[AIndex, i] := ALineClass.Create(FDropBoxPanel.Parent, i);
+      FLines[AIndex, i].Visible := False;
+    end;
 end;
 
 procedure TKMGUIGameSpectator.ChangePage(Sender: TObject);
-begin
-  if Assigned(FSelectedPanel) then
-    FSelectedPanel.Hide;
-
-  FSelectedPanel := TKMGUIGameSpectatorPanel(FPanelList[FDropBox.ItemIndex]);
-
-  if Assigned(FSelectedPanel) then
-  begin
-    FSelectedPanel.Activation;
-    FSelectedPanel.Show;
-  end;
+var
+  LineClass: TKMGUIGameSpectatorItemLineClass;
+  i: Integer;
+begin    
+  for i := 0 to MAX_LOBBY_PLAYERS - 1 do
+    if Assigned(FLines[FLastIndex, i]) then    
+      FLines[FLastIndex, i].Visible := False;
+                                      
+  FLastIndex := FDropBox.ItemIndex;
+  
+  for i := 0 to Min(MAX_LOBBY_PLAYERS, gHands.Count) - 1 do
+    if Assigned(FLines[FLastIndex, i]) then 
+      FLines[FLastIndex, i].Visible := True;
 end;
 
 end.
