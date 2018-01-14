a313ba2d17180f65693a5bdc23ae8d05663590bd
rey <levchenko.and@gmail.com>
2018-01-14 19:17:51 +0300

    fix HotKey set in Options menu for Up/Down and other navigating keys

--- src/KM_Controls.pas
+++ src/KM_Controls.pas
@@ -5873,6 +5873,8 @@ var
   OldIndex, NewIndex: Integer;
   PageScrolling: Boolean;
 begin
+  if PassAllKeys then Exit;
+  
   Result := (Key in [VK_UP, VK_DOWN, VK_HOME, VK_END, VK_PRIOR, VK_NEXT]) and not HideSelection;
   if inherited KeyDown(Key, Shift) then Exit;
 
