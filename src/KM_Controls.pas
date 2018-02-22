c52d6b98610cade733c1e0401fcd97cbb7474262
rey <levchenko.and@gmail.com>
2018-02-22 23:31:20 +0300

    Controls: add acHex for TKMEdit.Allowed chars

--- src/KM_Controls.pas
+++ src/KM_Controls.pas
@@ -488,7 +488,8 @@ type
 
   TKMAllowedChars = (
     acDigits, //Only 0..9 digits, for numeric input
-    acANSI7, //#33..#123,#125,#126 - only basic latin chars and symbols for user nikname, except |
+    acHex,    //Only 0..9 A..F a..f, for input hex values (colors)
+    acANSI7,  //#33..#123,#125,#126 - only basic latin chars and symbols for user nikname, except |
     acFileName, //Exclude symbols that can't be used in filenames
     acText,  //Anything is allowed except for eol symbol
     acAll    //Anything is allowed
@@ -2433,16 +2434,18 @@ end;
 // Check if specified aChar is allowed for specified aAllowedChars type
 function IsCharAllowed(aChar: WideChar; aAllowedChars: TKMAllowedChars): Boolean;
 const
-  Ansi7Chars: TSetOfAnsiChar = [#32..#123,#125..#126]; //except | character
-  NonFileChars: TSetOfAnsiChar = [#0 .. #31, '<', '>', #176, '|', '"', '\', '/', ':', '*', '?'];
+  HexDigits:        TSetOfAnsiChar = [#48..#57,#65..#70,#97..#102]; //0..9 A..F a..f
+  Ansi7Chars:       TSetOfAnsiChar = [#32..#123,#125..#126]; //except | character
+  NonFileChars:     TSetOfAnsiChar = [#0 .. #31, '<', '>', #176, '|', '"', '\', '/', ':', '*', '?'];
   NonTextCharsWEOL: TSetOfAnsiChar = [#0 .. #31, #176, '|']; //° has negative width so acts like a backspace in KaM fonts
-  NonTextChars: TSetOfAnsiChar = [#0 .. #31, #176]; //° has negative width so acts like a backspace in KaM fonts
+  NonTextChars:     TSetOfAnsiChar = [#0 .. #31, #176]; //° has negative width so acts like a backspace in KaM fonts
 begin
-  Result := not ((aAllowedChars = acDigits) and not InRange(Ord(aChar), 48, 57)
-    or (aAllowedChars = acANSI7) and not CharInSet(aChar, Ansi7Chars)
-    or (aAllowedChars = acFileName) and CharInSet(aChar, NonFileChars)
-    or (aAllowedChars = acText) and CharInSet(aChar, NonTextCharsWEOL)
-    or (aAllowedChars = acAll) and CharInSet(aChar, NonTextChars));
+  Result := not ((aAllowedChars = acDigits)   and not InRange(Ord(aChar), 48, 57)
+              or (aAllowedChars = acHex)      and not CharInSet(aChar, HexDigits)
+              or (aAllowedChars = acANSI7)    and not CharInSet(aChar, Ansi7Chars)
+              or (aAllowedChars = acFileName) and CharInSet(aChar, NonFileChars)
+              or (aAllowedChars = acText)     and CharInSet(aChar, NonTextCharsWEOL)
+              or (aAllowedChars = acAll)      and CharInSet(aChar, NonTextChars));
 end;
 
 
