ffe0a05ad796080d8fe8b23781bf420d85a75cfb
rey <levchenko.and@gmail.com>
2018-03-08 11:46:26 +0300

    Scripting Actions comments fixes, for wiki beautification

--- src/scripting/KM_ScriptingActions.pas
+++ src/scripting/KM_ScriptingActions.pas
@@ -990,15 +990,24 @@ end;
 
 //* Version: 7000+
 //* Add AI attack 
-//** aPlayer - playerID
-//** aRepeating - is attack repeating
-//** aDelay - attack delay
-//** aTotalMen - total soldiers to attack
-//** aMelleCount, aAntiHorseCount, aRangedCount, aMountedCount - soldiers groups count
-//** aRandomGroups - use random groups for attack
-//** aTarget - attack target of TKMAIAttackTarget type. possible values (att_ClosestUnit, att_ClosestBuildingFromArmy, att_ClosestBuildingFromStartPos, att_CustomPosition)
-//** aCustomPosition - TKMPoint for custom position of attack. Used if att_CustomPosition was set up as attack target
-//** Result: Attack ID, that could be used for attack remove in the future
+//** <b>aPlayer</b> - playerID
+//** <b>aRepeating</b> - is attack repeating
+//** <b>aDelay</b> - attack delay
+//** <b>aTotalMen</b> - total soldiers to attack
+//** <b>aMelleCount</b>, <b>aAntiHorseCount</b>, <b>aRangedCount</b>, <b>aMountedCount</b> - soldiers groups count
+//** <b>aRandomGroups</b> - use random groups for attack
+//** <b>aTarget</b> - attack target of TKMAIAttackTarget type. Possible values:
+//** <pre>TKMAIAttackTarget = (
+//**  attClosestUnit, //Closest enemy unit
+//**  attClosestBuildingFromArmy,
+//**    //Closest building from the group lauching the attack
+//**  attClosestBuildingFromStartPos,
+//**    //Closest building from the AI's start position
+//**  attCustomPosition
+//**    //Custom point defined with aCustomPosition
+//** );</pre>
+//** <b>aCustomPosition</b> - TKMPoint for custom position of attack. Used if att_CustomPosition was set up as attack target
+//** <b>Result</b>: Attack Id, that could be used to remove this attack later on
 function TKMScriptActions.AIAttackAdd(aPlayer: Byte; aRepeating: Boolean; aDelay: Cardinal; aTotalMen: Integer; aMelleCount, aAntiHorseCount, aRangedCount, aMountedCount: Word; aRandomGroups: Boolean; aTarget: TKMAIAttackTarget; aCustomPosition: TKMPoint): Integer;
 var
   AttackType: TKMAIAttackType;
@@ -2400,7 +2409,7 @@ end;
 
 //* Version: 7000+
 //* Sets array of tiles info, with possible change of
-//* 1. terrain (tile type), rotation (same as for MapTileSet),
+//* 1. terrain (tile type) and/or rotation (same as for MapTileSet),
 //* 2. tile height (same as for MapTileHeightSet)
 //* 3. tile object (same as for MapTileObjectSet)
 //* Works much faster, then applying all changes successively for every tile, because pathfinding compute is executed only once after all changes have been done
@@ -2413,7 +2422,8 @@ end;
 //*   ChangeSet: TKMTileChangeTypeSet; // Set of changes.
 //* end;
 //* TKMTileChangeTypeSet = set of TKMTileChangeType
-//* TKMTileChangeType = (tctTerrain, tctRotation, tctHeight, tctObject)</pre>
+//* TKMTileChangeType =
+//*   (tctTerrain, tctRotation, tctHeight, tctObject)</pre>
 //* ChangeSet determines what should be changed on tile
 //* F.e. if we want to change terrain type and height, then ChangeSet should contain tctTerrain and tctHeight
 //* Note: aTiles elements should start from 0, as for dynamic array. So f.e. to change map tile 1,1 we should set aTiles[0][0].
