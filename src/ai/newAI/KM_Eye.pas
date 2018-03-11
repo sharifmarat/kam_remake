a7aeaf8dd74feef6aa3a377a6ca865aa8f9be875
Poznamenany <32431877+Poznamenany@users.noreply.github.com>
2018-03-11 19:39:48 +0100

    Compilation fix

--- src/ai/newAI/KM_Eye.pas
+++ src/ai/newAI/KM_Eye.pas
@@ -919,7 +919,7 @@ begin
        AND not U.IsDeadOrDying
        AND (U.Owner >= 0) // Dont select animals!
        AND (U.Owner <> fOwner)
-       AND (gHands[fOwner].Alliances[U.Owner] <> at_Ally)) then
+       AND (gHands[fOwner].Alliances[U.Owner] <> at_Ally) then
       begin
         Distance := KMDistanceAbs(KMPoint(X,Y), aInitPoint);
         if (Closest > Distance) then
