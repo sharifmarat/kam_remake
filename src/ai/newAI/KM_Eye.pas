9e2875b1359ac3f32e3ae4d9baf0b0a09d4b69f9
Poznamenany <32431877+Poznamenany@users.noreply.github.com>
2018-03-11 18:20:44 +0100

    Aim animal fix

--- src/ai/newAI/KM_Eye.pas
+++ src/ai/newAI/KM_Eye.pas
@@ -917,6 +917,7 @@ begin
       U := gTerrain.UnitsHitTest(X,Y);
       if (U <> nil)
        AND not U.IsDeadOrDying
+       AND (U.Owner >= 0) // Dont select animals!
        AND ((U.Owner <> fOwner) OR (gHands[fOwner].Alliances[U.Owner] <> at_Ally)) then
       begin
         Distance := KMDistanceAbs(KMPoint(X,Y), aInitPoint);
