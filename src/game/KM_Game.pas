38668e09cf0339ea5f6ea24abf1a95444179bff1
Poznamenany <32431877+Poznamenany@users.noreply.github.com>
2018-04-01 22:02:19 +0200

    Implementation of Ray's latest commit

--- src/game/KM_Game.pas
+++ src/game/KM_Game.pas
@@ -34,7 +34,7 @@ type
     fIsPaused: Boolean;
     fGameSpeed: Single; //Actual speedup value
     fGameSpeedMultiplier: Word; //How many ticks are compressed into one
-    fGameMode: TGameMode;
+    fGameMode: TKMGameMode;
     fWaitingForNetwork: Boolean; //Indicates that we are waiting for other players commands in MP
     fAdvanceFrame: Boolean; //Replay variable to advance 1 frame, afterwards set to false
     fSaveFile: UnicodeString;  //Relative pathname to savegame we are playing, so it gets saved to crashreport
@@ -67,7 +67,7 @@ type
     procedure MultiplayerRig;
     procedure SaveGame(const aPathName: UnicodeString; aTimestamp: TDateTime; const aMinimapPathName: UnicodeString = '');
     procedure UpdatePeaceTime;
-    function WaitingPlayersList: TKMByteArray;
+    function GetWaitingPlayersList: TKMByteArray;
     function FindHandToSpec: Integer;
     procedure UpdateTickCounters;
     function GetTicksBehindCnt: Single;
@@ -92,7 +92,7 @@ type
     ///	<param name="aNetworking">
     ///	  Pointer to networking class, required if this is a multiplayer game.
     ///	</param>
-    constructor Create(aGameMode: TGameMode; aRender: TRender; aNetworking: TKMNetworking);
+    constructor Create(aGameMode: TKMGameMode; aRender: TRender; aNetworking: TKMNetworking);
     destructor Destroy; override;
 
     procedure GameStart(const aMissionFile, aGameName: UnicodeString; aCRC: Cardinal; aCampaign: TKMCampaign;
@@ -155,7 +155,7 @@ type
     property ControlledHandIndex: TKMHandIndex read GetControlledHandIndex;
 
     property Scripting: TKMScripting read fScripting;
-    property GameMode: TGameMode read fGameMode;
+    property GameMode: TKMGameMode read fGameMode;
     property SaveFile: UnicodeString read fSaveFile;
     function GetMissionFile: UnicodeString;
     function GetScriptSoundFile(const aSound: AnsiString; aAudioFormat: TKMAudioFormat): UnicodeString;
@@ -219,9 +219,9 @@ uses
 //Create template for the Game
 //aRender - who will be rendering the Game session
 //aNetworking - access to MP stuff
-constructor TKMGame.Create(aGameMode: TGameMode; aRender: TRender; aNetworking: TKMNetworking);
+constructor TKMGame.Create(aGameMode: TKMGameMode; aRender: TRender; aNetworking: TKMNetworking);
 const
-  UIMode: array[TGameMode] of TUIMode = (umSP, umSP, umMP, umSpectate, umSP, umReplay, umReplay);
+  UIMode: array[TKMGameMode] of TUIMode = (umSP, umSP, umMP, umSpectate, umSP, umReplay, umReplay);
 begin
   inherited Create;
 
@@ -337,7 +337,7 @@ end;
 procedure TKMGame.GameStart(const aMissionFile, aGameName: UnicodeString; aCRC: Cardinal; aCampaign: TKMCampaign;
                             aCampMap: Byte; aLocation: ShortInt; aColor: Cardinal; aMapDifficulty: TKMMissionDifficulty = mdNone);
 const
-  GAME_PARSE: array [TGameMode] of TKMMissionParsingMode = (
+  GAME_PARSE: array [TKMGameMode] of TKMMissionParsingMode = (
     mpm_Single, mpm_Single, mpm_Multi, mpm_Multi, mpm_Editor, mpm_Single, mpm_Single);
 var
   I: Integer;
@@ -902,7 +902,7 @@ end;
 
 
 //Get list of players we are waiting for. We do it here because fNetworking does not knows about GIP
-function TKMGame.WaitingPlayersList: TKMByteArray;
+function TKMGame.GetWaitingPlayersList: TKMByteArray;
 var
   ErrorMsg: UnicodeString;
 begin
@@ -914,10 +914,10 @@ begin
         //We are waiting during inital loading
         Result := fNetworking.NetPlayers.GetNotReadyToPlayPlayers;
     else  begin
-            ErrorMsg := 'WaitingPlayersList from wrong state: '
+            ErrorMsg := 'GetWaitingPlayersList from wrong state: '
                        + GetEnumName(TypeInfo(TNetGameState), Integer(fNetworking.NetGameState));
             gLog.AddTime(ErrorMsg);
-            raise Exception.Create(ErrorMsg);
+            //raise Exception.Create(ErrorMsg); //This error sometimes occur when host quits, but that's not critical, so we can just log it
           end;
   end;
 end;
@@ -926,13 +926,13 @@ end;
 procedure TKMGame.WaitingPlayersDisplay(aWaiting: Boolean);
 begin
   fWaitingForNetwork := aWaiting;
-  fGamePlayInterface.ShowNetworkLag(aWaiting, WaitingPlayersList, fNetworking.IsHost);
+  fGamePlayInterface.ShowNetworkLag(aWaiting, GetWaitingPlayersList, fNetworking.IsHost);
 end;
 
 
 procedure TKMGame.WaitingPlayersDrop;
 begin
-  fNetworking.DropPlayers(WaitingPlayersList);
+  fNetworking.DropPlayers(GetWaitingPlayersList);
 end;
 
 
