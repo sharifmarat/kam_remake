11ba5b18c012638b565e48ce55a4c238ac4f0bce
rey <levchenko.and@gmail.com>
2019-04-15 14:40:03 +0300

    add RESET_DEBUG_CONTROLS AGAIN
    3rd time!

--- src/KM_FormMain.pas
+++ src/KM_FormMain.pas
@@ -645,6 +645,9 @@ procedure TFormMain.ControlsReset;
         ResetGroupBox(TGroupBox(aBox.Controls[I]));
   end;
 begin
+  if not RESET_DEBUG_CONTROLS then
+    Exit;
+
   fUpdating := True;
   ResetGroupBox(GroupBox1);
 
