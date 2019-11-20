8949c3deddafc9b9660ee0fa83f3d18ee3b37068
rey <levchenko.and@gmail.com>
2019-11-20 17:36:37 +0200

    update clean.bat

--- bat/clean_src.bat
+++ bat/clean_src.bat
@@ -22,7 +22,7 @@ erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S /A:H *.o *.or *.ppu *.compiled *.local
 erase /F /Q /S /A:H *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi *.LOG.txt *.stat bugreport.txt *.bak
 
-erase /F /Q /S Maps\*.rar Maps\*.zip Maps\*.7z MapsMP\*.rar MapsMP\*.zip MapsMP\*.7z
+erase /F /Q /S Maps\*.rar Maps\*.zip Maps\*.7z MapsMP\*.rar MapsMP\*.zip MapsMP\*.7z Campaigns\*.rar Campaigns\*.zip Campaigns\*.7z
 
 REM clean all empty dirs, f.e. old/renamed maps
 ROBOCOPY . . /S /MOVE
