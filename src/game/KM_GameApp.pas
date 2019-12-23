fc99df0ba36f773b2903d86075ef5b7ee36cdac0
rey <levchenko.and@gmail.com>
2019-12-23 04:09:50 +0200

    Fix campaign progress was not saved properly

--- src/game/KM_GameApp.pas
+++ src/game/KM_GameApp.pas
@@ -480,14 +480,14 @@ begin
       fCampaigns.ActiveCampaign.ScriptData.Clear;
       gGame.SaveCampaignScriptData(fCampaigns.ActiveCampaign.ScriptData);
 
+      if aMsg = grWin then
+        fCampaigns.UnlockNextMap; //Unlock next map before save campaign progress
+
       //Always save Campaigns progress after mission
       //We always save script data with SaveCampaignScriptData
       //then campaign progress should be saved always as well on any outcome (win or lose)
       //otherwise we will get inconsistency
       SaveCampaignsProgress;
-
-      if aMsg = grWin then
-        fCampaigns.UnlockNextMap;
     end;
   end;
 
