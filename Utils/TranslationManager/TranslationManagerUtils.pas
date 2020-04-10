e4e82ae88ff717d1324001cb98574f0e6c3082ba
rey <levchenko.and@gmail.com>
2020-04-10 06:07:35 +0300

    Add Utils as a working dir for Translation manager

--- Utils/TranslationManager/TranslationManagerUtils.pas
+++ Utils/TranslationManager/TranslationManagerUtils.pas
@@ -12,17 +12,22 @@ uses
 
 function GetWorkDir(aShowWarningMess: Boolean = False): UnicodeString;
 var
-  ExeDir, WorkDir: UnicodeString;
+  ExeDir, ProjectDir, WorkDir: UnicodeString;
 begin
   Result := '';
-	ExeDir := ExtractFilePath(ParamStr(0));
-  WorkDir := ExeDir + '..\..\'; //If start TranslationManager from Utils/TranslationManager folder
+	ExeDir := ExtractFilePath(ParamStr(0)); //if starts from kam_remake folder
+  WorkDir := ExeDir + '..\'; // If starts from kam_remake/Utils
+  ProjectDir := ExeDir + '..\..\'; //If start TranslationManager from kam_remake/Utils/TranslationManager folder
 
   if not FileExists(WorkDir + 'data\locales.txt') then
   begin
     if FileExists(ExeDir + 'data\locales.txt') then
       WorkDir := ExeDir //If start TranslationManager from KMR folder
-    else begin
+    else
+    if FileExists(ProjectDir + 'data\locales.txt') then
+      WorkDir := ProjectDir
+    else
+    begin
       if aShowWarningMess then
         ShowMessage('Can''t find locales.txt file at destinations:' + #13#10
           + ExeDir + 'data\locales.txt' + #13#10
