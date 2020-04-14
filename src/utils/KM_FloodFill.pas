571de743ec48eb8d7cd5ac29dc32f759f62adab6
Poznamenany <poznamenany@gmail.com>
2020-04-14 19:59:54 +0200

    KM_FloodFill - fixed memory leak

--- src/utils/KM_FloodFill.pas
+++ src/utils/KM_FloodFill.pas
@@ -38,133 +38,146 @@ type
 
 implementation
 
+
 { TKMQuickFlood }
-  constructor TKMQuickFlood.Create(const aScanEightTiles: Boolean = False);
-  begin
-    fScanEightTiles := aScanEightTiles;
-    fStartQueue := nil;
-    fEndQueue := nil;
-  end;
+constructor TKMQuickFlood.Create(const aScanEightTiles: Boolean = False);
+begin
+  fScanEightTiles := aScanEightTiles;
+  // Create first TElement and keep it all the time (queue is empty if fStartQueue = fEndQueue)
+  New(fStartQueue);
+  fStartQueue^.Next := nil;
+  fEndQueue := fStartQueue;
+end;
 
-  destructor TKMQuickFlood.Destroy();
-  var
-    X,Y: SmallInt;
-  begin
-    while not IsQueueEmpty do
-      RemoveFromQueue(X,Y);
-    inherited;
-  end;
 
-  procedure TKMQuickFlood.MakeNewQueue();
-  begin
-    new(fStartQueue);
-    fStartQueue^.Next := nil;
-    fEndQueue := fStartQueue;
-  end;
+destructor TKMQuickFlood.Destroy();
+var
+  X,Y: SmallInt;
+begin
+  while not IsQueueEmpty do // Dispose everything except 1 last element
+    RemoveFromQueue(X,Y);
+  Dispose(fStartQueue); // Dispose the last element
+  inherited;
+end;
 
-  procedure TKMQuickFlood.InsertInQueue(const aX, aY: SmallInt);
-  begin
-    fEndQueue^.X := aX;
-    fEndQueue^.Y := aY;
-    new(fEndQueue^.Next);
-    fEndQueue := fEndQueue^.Next;
-    fEndQueue^.Next := nil;
-  end;
 
-  function TKMQuickFlood.RemoveFromQueue(var aX, aY: SmallInt): Boolean;
-  var pom: TElementPointer;
+procedure TKMQuickFlood.MakeNewQueue();
+var
+  X,Y: SmallInt;
+begin
+  while not IsQueueEmpty do // Make sure that queue is empty
+    RemoveFromQueue(X,Y);
+end;
+
+
+procedure TKMQuickFlood.InsertInQueue(const aX, aY: SmallInt);
+begin
+  fEndQueue^.X := aX;
+  fEndQueue^.Y := aY;
+  new(fEndQueue^.Next);
+  fEndQueue := fEndQueue^.Next;
+  fEndQueue^.Next := nil;
+end;
+
+
+function TKMQuickFlood.RemoveFromQueue(var aX, aY: SmallInt): Boolean;
+var pom: TElementPointer;
+begin
+  Result := True;
+  if IsQueueEmpty then
   begin
-    Result := True;
-    if IsQueueEmpty then
+    Result := False;
+  end
+  else
+  begin
+    aX := fStartQueue^.X;
+    aY := fStartQueue^.Y;
+    pom := fStartQueue;
+    fStartQueue := fStartQueue^.Next;
+    Dispose(pom);
+  end;
+end;
+
+
+function TKMQuickFlood.IsQueueEmpty: Boolean;
+begin
+  Result := fStartQueue = fEndQueue;
+end;
+
+
+procedure TKMQuickFlood.SearchAround(aX,aY: SmallInt);
+
+  procedure ScanInColumn(const aX,aY: SmallInt; var PreviousStep: Boolean);
+  begin
+    if PreviousStep then
+      PreviousStep := CanBeVisited(aX,aY)
+    else if not IsVisited(aX,aY) AND CanBeVisited(aX,aY) then
     begin
-      Result := False;
-    end
-    else
-    begin
-      aX := fStartQueue^.X;
-      aY := fStartQueue^.Y;
-      pom := fStartQueue;
-      fStartQueue := fStartQueue^.Next;
-      Dispose(pom);
-    end;
-  end;
-
-  function TKMQuickFlood.IsQueueEmpty: Boolean;
-  begin
-    Result := fStartQueue = fEndQueue;
-  end;
-
-  procedure TKMQuickFlood.SearchAround(aX,aY: SmallInt);
-
-    procedure ScanInColumn(const aX,aY: SmallInt; var PreviousStep: Boolean);
-    begin
-      if PreviousStep then
-        PreviousStep := CanBeVisited(aX,aY)
-      else if not IsVisited(aX,aY) AND CanBeVisited(aX,aY) then
-      begin
-        PreviousStep := True;
-        InsertInQueue(aX, aY);
-      end;
-    end;
-
-    procedure ScanInRow(const aX,aY, Dir: SmallInt; const CheckActualPosition: Boolean = False);
-    var
-      X,Y_T,Y_D: SmallInt;
-      Top,Down, TopCheck,DownCheck: Boolean;
-    begin
-      Top := aY > fMinLimit.Y;
-      Down := aY < fMaxLimit.Y;
-      Y_T := aY - 1;
-      Y_D := aY + 1;
-      TopCheck := False;
-      DownCheck := False;
-      if Top then
-        TopCheck := CanBeVisited(aX,Y_T) AND not IsVisited(aX, Y_T);
-      if Down then
-        DownCheck := CanBeVisited(aX,Y_D) AND not IsVisited(aX, Y_D);
-      if CheckActualPosition then
-      begin
-        if TopCheck then
-          InsertInQueue(aX, Y_T);
-        if DownCheck then
-          InsertInQueue(aX, Y_D);
-      end;
-      X := aX + Dir;
-      while (X >= fMinLimit.X) AND (X <= fMaxLimit.X) AND CanBeVisited(X,aY) do
-      begin
-        MarkAsVisited(X, aY);
-        if Top then
-          ScanInColumn(X,Y_T, TopCheck);
-        if Down then
-          ScanInColumn(X,Y_D, DownCheck);
-        X := X + Dir;
-      end;
-      if fScanEightTiles AND (X >= fMinLimit.X) AND (X <= fMaxLimit.X) then
-      begin
-        if Top then
-          ScanInColumn(X,Y_T, TopCheck);
-        if Down then
-          ScanInColumn(X,Y_D, DownCheck);
-      end;
-    end;
-
-  begin
-    MarkAsVisited(aX, aY);
-    ScanInRow(aX,aY, 1, True);
-    ScanInRow(aX,aY, -1, False);
-  end;
-
-  procedure TKMQuickFlood.QuickFlood(aX,aY: SmallInt);
-  begin
-    MakeNewQueue();
-    if CanBeVisited(aX, aY) then
+      PreviousStep := True;
       InsertInQueue(aX, aY);
-    while not IsQueueEmpty do
-    begin
-      RemoveFromQueue(aX, aY);
-      if not IsVisited(aX, aY) then
-        SearchAround(aX, aY);
     end;
   end;
+
+  procedure ScanInRow(const aX,aY, Dir: SmallInt; const CheckCurrentPosition: Boolean = False);
+  var
+    X,Y_T,Y_D: SmallInt;
+    Top,Down, TopCheck,DownCheck: Boolean;
+  begin
+    Top := aY > fMinLimit.Y;
+    Down := aY < fMaxLimit.Y;
+    Y_T := aY - 1;
+    Y_D := aY + 1;
+    TopCheck := False;
+    DownCheck := False;
+    if Top then
+      TopCheck := CanBeVisited(aX,Y_T) AND not IsVisited(aX, Y_T);
+    if Down then
+      DownCheck := CanBeVisited(aX,Y_D) AND not IsVisited(aX, Y_D);
+    if CheckCurrentPosition then
+    begin
+      if TopCheck then
+        InsertInQueue(aX, Y_T);
+      if DownCheck then
+        InsertInQueue(aX, Y_D);
+    end;
+    X := aX + Dir;
+    while (X >= fMinLimit.X) AND (X <= fMaxLimit.X) AND CanBeVisited(X,aY) do
+    begin
+      MarkAsVisited(X, aY);
+      if Top then
+        ScanInColumn(X,Y_T, TopCheck);
+      if Down then
+        ScanInColumn(X,Y_D, DownCheck);
+      X := X + Dir;
+    end;
+    if fScanEightTiles AND (X >= fMinLimit.X) AND (X <= fMaxLimit.X) then
+    begin
+      if Top then
+        ScanInColumn(X,Y_T, TopCheck);
+      if Down then
+        ScanInColumn(X,Y_D, DownCheck);
+    end;
+  end;
+
+begin
+  MarkAsVisited(aX, aY);
+  ScanInRow(aX,aY, 1, True);
+  ScanInRow(aX,aY, -1, False);
+end;
+
+
+procedure TKMQuickFlood.QuickFlood(aX,aY: SmallInt);
+begin
+  MakeNewQueue();
+  if CanBeVisited(aX, aY) then
+    InsertInQueue(aX, aY);
+  while not IsQueueEmpty do
+  begin
+    RemoveFromQueue(aX, aY);
+    if not IsVisited(aX, aY) then
+      SearchAround(aX, aY);
+  end;
+end;
+
 
 end.
